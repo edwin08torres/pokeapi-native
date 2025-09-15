e8f005bba3ef209807b4f0aedb091f995b085ff2
edwin08torres <at2899743@gmail.com>
2025-09-15 17:32:18 -0600

    fix config

--- babel.config.js
+++ babel.config.js
@@ -2,25 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      [
-        "module-resolver",
-        {
-          root: ["./"],
-          extensions: [".tsx", ".ts", ".js", ".json"],
-          alias: {
-            "@": "./src",
-            "@api": "./src/api",
-            "@components": "./src/components",
-            "@hooks": "./src/hooks",
-            "@screens": "./src/screens",
-            "@store": "./src/store",
-            "@theme": "./src/theme",
-            "@utils": "./src/utils",
-          },
-        },
-      ],
-      "react-native-reanimated/plugin",
-    ],
+    plugins: [],
   };
 };
